4e5d6cc0996bfcf62036ee82b289acf6a1febe2f
Pawel Panek <p.panek@samsung.com>
2018-01-29 11:08:31 +0100

    Implement ConfigParser for logging framework
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

---
+++ JuvoPlayer.Common/Logging/LogLevel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuvoPlayer.Common.Logging
+{
+    public enum LogLevel
+    {
+        Fatal,
+        Error,
+        Warn,
+        Info,
+        Debug,
+        Verbose
+    }
+}
---
+++ JuvoPlayer.Tests/testcase/TSConfigParser.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using JuvoPlayer.Common.Logging;
+using NUnit.Framework;
+
+namespace JuvoPlayer.Tests
+{
+    [TestFixture]
+    class TSConfigParser
+    {
+        private string LoadConfigFile(string filename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var names = assembly.GetManifestResourceNames();
+            foreach (var name in names)
+            {
+                if (!name.Contains(filename)) continue;
+                var stream = assembly.GetManifestResourceStream(name);
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            return string.Empty;
+        }
+
+        [Test]
+        public void TestValidConfig()
+        {
+            var contents = LoadConfigFile("logging_valid.config");
+            Assert.That(contents, Is.Not.Empty);
+
+            var parser = new ConfigParser(contents);
+
+            var expectedValues = new Dictionary<string, LogLevel>
+            {
+                {"JuvoPlayer", LogLevel.Debug},
+                {"JuvoPlayer.Common", LogLevel.Warn},
+                {"XamarinPlayer.Tizen.TV", LogLevel.Info}
+            };
+
+            Assert.That(parser.LoggingLevels.Count, Is.EqualTo(expectedValues.Count));
+
+            foreach (var pair in expectedValues)
+            {
+                Assert.That(parser.LoggingLevels.ContainsKey(pair.Key), Is.True);
+                Assert.That(parser.LoggingLevels[pair.Key], Is.EqualTo(pair.Value));
+            }
+        }
+
+        [Test]
+        public void TestInvalidConfig()
+        {
+            var contents = LoadConfigFile("logging_invalid.config");
+            Assert.That(contents, Is.Not.Empty);
+
+            var parser = new ConfigParser(contents);
+            Assert.That(parser.LoggingLevels, Is.Empty);
+        }
+    }
+}
