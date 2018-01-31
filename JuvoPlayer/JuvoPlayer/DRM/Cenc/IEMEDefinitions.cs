1d8e57484e342e714d487fcaa13c39717717dae2
Pawel Panek <p.panek@samsung.com>
2018-01-31 13:46:21 +0100

    Normalize line endings
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

--- JuvoPlayer/JuvoPlayer/DRM/Cenc/IEMEDefinitions.cs
+++ JuvoPlayer/JuvoPlayer/DRM/Cenc/IEMEDefinitions.cs
@@ -6,8 +6,8 @@ using System.Text;
 
 namespace JuvoPlayer.DRM.Cenc
 {
-    public unsafe struct DecryptionData    {        public bool IsEncrypted;        public bool IsSecure;        public byte[] KeyId;        public byte[] EncryptData;        public byte[] Iv;    };
-    [StructLayout(LayoutKind.Sequential, Pack = 8)]    public struct MSD_SUBSAMPLE_INFO    {        public uint uBytesOfClearData;        public uint uBytesOfEncryptedData;    };
+    public unsafe struct DecryptionData    {        public bool IsEncrypted;        public bool IsSecure;        public byte[] KeyId;        public byte[] EncryptData;        public byte[] Iv;    };
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]    public struct MSD_SUBSAMPLE_INFO    {        public uint uBytesOfClearData;        public uint uBytesOfEncryptedData;    };
     [StructLayout(LayoutKind.Sequential, Pack = 8)]    public struct MSD_FMP4_DATA    {        public uint uSubSampleCount;        public IntPtr pSubSampleInfo;
     };
 }
