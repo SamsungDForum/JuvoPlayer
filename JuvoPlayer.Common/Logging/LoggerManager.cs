774e87ca26ca7dd4c074c5dbf4a338a42f4a4fea
Pawel Panek <p.panek@samsung.com>
2018-02-01 07:43:32 +0100

    Fix coding style in LoggerManager class
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

--- JuvoPlayer.Common/Logging/LoggerManager.cs
+++ JuvoPlayer.Common/Logging/LoggerManager.cs
@@ -6,62 +6,62 @@ namespace JuvoPlayer.Common.Logging
 {
     public class LoggerManager
     {
-        private static LoggerManager _instance;
+        private static LoggerManager Instance;
 
-        private readonly Dictionary<string, LogLevel> _loggingLevels;
-        private readonly CreateLoggerFunc _createLoggerFunc;
-        private readonly List<LoggerBase> _loggers = new List<LoggerBase>();
+        private readonly Dictionary<string, LogLevel> loggingLevels;
+        private readonly CreateLoggerFunc createLoggerFunc;
+        private readonly List<LoggerBase> loggers = new List<LoggerBase>();
         private static readonly LogLevel DefaultLogLevel = LogLevel.Debug;
 
         public delegate LoggerBase CreateLoggerFunc(string channel, LogLevel level);
 
         protected LoggerManager(Dictionary<string, LogLevel> loggingLevels, CreateLoggerFunc createLoggerFunc)
         {
-            _loggingLevels = loggingLevels;
-            _createLoggerFunc = createLoggerFunc;
+            this.loggingLevels = loggingLevels;
+            this.createLoggerFunc = createLoggerFunc;
         }
 
         public static LoggerManager ResetForTests()
         {
-            LoggerManager instance = _instance;
-            _instance = null;
+            LoggerManager instance = Instance;
+            Instance = null;
             return instance;
         }
 
         public static void RestoreForTests(LoggerManager instance)
         {
-            _instance = instance;
+            Instance = instance;
         }
 
         public static void Configure(CreateLoggerFunc createLoggerFunc)
         {
             if (createLoggerFunc == null)
                 throw new ArgumentNullException();
-            if (_instance != null)
+            if (Instance != null)
                 throw new InvalidOperationException("LoggerManager is already configured");
 
-            _instance = new LoggerManager(new Dictionary<string, LogLevel>(), createLoggerFunc);
+            Instance = new LoggerManager(new Dictionary<string, LogLevel>(), createLoggerFunc);
         }
 
         public static void Configure(string configData, CreateLoggerFunc createLoggerFunc)
         {
             if (configData == null || createLoggerFunc == null)
                 throw new ArgumentNullException();
-            if (_instance != null)
+            if (Instance != null)
                 throw new InvalidOperationException("LoggerManager is already configured");
 
             var configParser = new ConfigParser(configData);
-            _instance = new LoggerManager(configParser.LoggingLevels, createLoggerFunc);
+            Instance = new LoggerManager(configParser.LoggingLevels, createLoggerFunc);
         }
 
         public static void Configure(Stream configStream, CreateLoggerFunc createLoggerFunc)
         {
             if (configStream == null || createLoggerFunc == null)
                 throw new ArgumentNullException();
-            if (_instance != null)
+            if (Instance != null)
                 throw new InvalidOperationException("LoggerManager is already configured");
 
-            var configData = string.Empty;
+            string configData;
             using (var reader = new StreamReader(configStream))
             {
                 configData = reader.ReadToEnd();
@@ -72,25 +72,25 @@ namespace JuvoPlayer.Common.Logging
 
         public static LoggerManager GetInstance()
         {
-            if (_instance == null)
+            if (Instance == null)
                 throw new InvalidOperationException("LoggerManager.Configure() should be called before GetInstance()");
-            return _instance;
+            return Instance;
         }
 
         public ILogger GetLogger(string channel)
         {
-            foreach (var logger in _loggers)
+            foreach (var logger in loggers)
             {
                 if (logger.Channel.Equals(channel))
                     return logger;
             }
 
             LogLevel newLogLevel = DefaultLogLevel;
-            if (_loggingLevels.ContainsKey(channel))
-                newLogLevel = _loggingLevels[channel];
+            if (loggingLevels.ContainsKey(channel))
+                newLogLevel = loggingLevels[channel];
 
-            LoggerBase newLogger = _createLoggerFunc(channel, newLogLevel);
-            _loggers.Add(newLogger);
+            LoggerBase newLogger = createLoggerFunc(channel, newLogLevel);
+            loggers.Add(newLogger);
             return newLogger;
         }
     }
