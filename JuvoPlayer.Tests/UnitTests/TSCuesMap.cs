689448bd41c314960e6368a2c992f0b54a52a695
Pawel Panek <p.panek@samsung.com>
2018-03-21 12:33:19 +0100

    Implement CuesMap
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

---
+++ JuvoPlayer.Tests/UnitTests/TSCuesMap.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using JuvoPlayer.Subtitles;
+using NUnit.Framework;
+
+namespace JuvoPlayer.Tests.UnitTests
+{
+    [TestFixture]
+    class TSCuesMap
+    {
+        private static CuesMap CreateCuesMap()
+        {
+            CuesMap map = new CuesMap();
+            return map;
+        }
+
+        [Test]
+        public void Get_MapIsEmpty_ReturnsNull()
+        {
+            var map = CreateCuesMap();
+
+            var received = map.Get(TimeSpan.FromSeconds(60));
+
+            Assert.That(received, Is.Null);
+        }
+
+        [Test]
+        public void Get_ValidKey_ReturnsCue()
+        {
+            var map = CreateCuesMap();
+            Cue cue = new Cue()
+            {
+                Begin = TimeSpan.FromSeconds(10),
+                End = TimeSpan.FromSeconds(20)
+            };
+
+            map.Put(cue);
+            var received = map.Get(TimeSpan.FromSeconds(15));
+
+            Assert.That(received.Begin, Is.EqualTo(TimeSpan.FromSeconds(10)));
+            Assert.That(received.End, Is.EqualTo(TimeSpan.FromSeconds(20)));
+        }
+
+        [Test]
+        public void Get_KeyBetweenTwoCues_ReturnsNull()
+        {
+            var map = CreateCuesMap();
+
+            Cue first = new Cue()
+            {
+                Begin = TimeSpan.FromSeconds(10),
+                End = TimeSpan.FromSeconds(20)
+            };
+            map.Put(first);
+
+            Cue second = new Cue()
+            {
+                Begin = TimeSpan.FromSeconds(30),
+                End = TimeSpan.FromSeconds(40)
+            };
+            map.Put(second);
+
+            var received = map.Get(TimeSpan.FromSeconds(25));
+
+            Assert.That(received, Is.Null);
+        }
+
+        [Test]
+        public void Get_KeyForSecondCue_ReturnsSecondCue()
+        {
+            var map = CreateCuesMap();
+
+            Cue first = new Cue()
+            {
+                Begin = TimeSpan.FromSeconds(10),
+                End = TimeSpan.FromSeconds(20)
+            };
+            map.Put(first);
+
+            Cue second = new Cue()
+            {
+                Begin = TimeSpan.FromSeconds(30),
+                End = TimeSpan.FromSeconds(40)
+            };
+            map.Put(second);
+
+            var received = map.Get(TimeSpan.FromSeconds(30));
+
+            Assert.That(received.Begin, Is.EqualTo(TimeSpan.FromSeconds(30)));
+            Assert.That(received.End, Is.EqualTo(TimeSpan.FromSeconds(40)));
+        }
+    }
+}
---
+++ JuvoPlayer/Subtitles/CuesMap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuvoPlayer.Subtitles
+{
+    internal class CuesMap
+    {
+        private List<Cue> cues = new List<Cue>();
+
+        public int Count => cues.Count;
+
+        public int Rank(TimeSpan key, int lo, int hi)
+        {
+            if (hi < lo) return lo;
+            int mid = lo + (hi - lo) / 2;
+            int cmp = cues[mid].Compare(key);
+            if (cmp < 0)
+                return Rank(key, lo, mid - 1);
+            if (cmp > 0)
+                return Rank(key, mid + 1, hi);
+            return mid;
+        }
+
+        public int Rank(TimeSpan key)
+        {
+            return Rank(key, 0, cues.Count - 1);
+        }
+
+        public void Put(Cue cue)
+        {
+            int i = Rank(cue.Begin);
+            if (i < Count && cues[i].Compare(cue.Begin) == 0)
+            {
+                cues[i] = cue;
+                return;
+            }
+            cues.Insert(i, cue);
+        }
+
+        public Cue Get(TimeSpan key)
+        {
+            if (Count == 0)
+                return null;
+            int i = Rank(key);
+            if (i < Count && cues[i].Compare(key) == 0)
+                return cues[i];
+            return null;
+        }
+    }
+}
