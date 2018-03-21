ed8af7fb25784ca2bc50894bfd5e7743567d8f27
Pawel Panek <p.panek@samsung.com>
2018-03-21 10:01:13 +0100

    Cue's improvements
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

---
+++ JuvoPlayer.Tests/UnitTests/TSCue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JuvoPlayer.Subtitles;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace JuvoPlayer.Tests.UnitTests
+{
+    [TestFixture]
+    class TSCue
+    {
+        [Test]
+        public void Begin_GreaterThanEnd_ThrowsArgumentException()
+        {
+            Cue cue = new Cue();
+            cue.End = TimeSpan.FromMinutes(10);
+
+            Assert.Throws<ArgumentException>(() => { cue.Begin = TimeSpan.FromMinutes(20);
+            });
+        }
+
+        [Test]
+        public void Begin_EqualsEnd_ThrowsArgumentException()
+        {
+            Cue cue = new Cue
+            {
+                End = TimeSpan.FromMinutes(10)
+            };
+
+            Assert.Throws<ArgumentException>(() => {
+                cue.Begin = TimeSpan.FromMinutes(10);
+            });
+        }
+
+        [Test]
+        public void End_LessThanBegin_ThrowsArgumentException()
+        {
+            Cue cue = new Cue {Begin = TimeSpan.FromMinutes(20)};
+
+            Assert.Throws<ArgumentException>(() => {
+                cue.End = TimeSpan.FromMinutes(10);
+            });
+        }
+
+        [Test]
+        public void End_EqualsBegin_ThrowsArgumentException()
+        {
+            Cue cue = new Cue { Begin = TimeSpan.FromMinutes(10) };
+
+            Assert.Throws<ArgumentException>(() => {
+                cue.End = TimeSpan.FromMinutes(10);
+            });
+        }
+
+        [Test]
+        public void End_GreaterThanBegin_SetsSuccessfully()
+        {
+            Cue cue = new Cue { Begin = TimeSpan.FromMinutes(10) };
+
+            cue.End = TimeSpan.FromMinutes(20);
+            Assert.That(cue.End, Is.EqualTo(TimeSpan.FromMinutes(20)));
+        }
+
+        [Test]
+        public void Begin_LessThanEnd_SetsSuccessfully()
+        {
+            Cue cue = new Cue { End = TimeSpan.FromMinutes(20) };
+
+            cue.Begin = TimeSpan.FromMinutes(10);
+            Assert.That(cue.Begin, Is.EqualTo(TimeSpan.FromMinutes(10)));
+        }
+    }
+}
--- JuvoPlayer/Subtitles/Cue.cs
+++ JuvoPlayer/Subtitles/Cue.cs
@@ -4,10 +4,61 @@ using System.Text;
 
 namespace JuvoPlayer.Subtitles
 {
+    /// <summary>Represents a single cue block.</summary>
     public class Cue
     {
+        private TimeSpan begin;
+        private TimeSpan end;
+
         public string Text { get; set; }
-        public TimeSpan Begin { get; set; }
-        public TimeSpan End { get; set; }
+
+        /// <summary>A timestamp representing the begin time offset of the cue. The time represented by this timestamp
+        /// must be less than end time offset of the cue.</summary>
+        /// <exception cref="T:System.ArgumentException">Begin time offset is greater than end time offset</exception>
+        public TimeSpan Begin
+        {
+            get => begin;
+            set
+            {
+                if (!VerifyTimeStamps(value, End))
+                    throw new ArgumentException("Invalid timestamp range");
+                begin = value;
+            }
+        }
+
+        /// <summary>A timestamp representing the end time offset of the cue. The time represented by this timestamp
+        /// must be greater than begin time offset of the cue.</summary>
+        /// <exception cref="T:System.ArgumentException">End time offset is less than begin time offset</exception>
+        public TimeSpan End
+        {
+            get => end;
+            set
+            {
+                if (!VerifyTimeStamps(Begin, value))
+                    throw new ArgumentException("Invalid timestamp range");
+                end = value;
+            }
+        }
+
+        /// <summary>Compares a given <see cref="T:System.TimeSpan"></see> object with current cue.</summary>
+        /// <param name="time">A time to compare</param>
+        /// <returns>Returns -1 when given <paramref name="time">time</paramref> is less than Cue's begin.
+        /// Returns 1 when given <paramref name="time">time</paramref> is greater than Cue's end.
+        /// Otherwise returns 0.</returns>
+        public int Compare(TimeSpan time)
+        {
+            if (time < Begin) return -1;
+            if (time > End) return 1;
+            return 0;
+        }
+
+        private static bool VerifyTimeStamps(TimeSpan begin, TimeSpan end)
+        {
+            if (begin == TimeSpan.Zero || end == TimeSpan.Zero)
+                return true;
+            if (end <= begin)
+                return false;
+            return begin <= end;
+        }
     }
 }
