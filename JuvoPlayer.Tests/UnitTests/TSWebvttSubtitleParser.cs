3b071ec12de99bb4ae5ff12b8da14b161298080d
Pawel Panek <p.panek@samsung.com>
2018-03-29 15:12:28 +0200

    WebVttSubtitleParser - parse time
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

---
+++ JuvoPlayer.Tests/UnitTests/TSWebvttSubtitleParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JuvoPlayer.Subtitles;
+using NUnit.Framework;
+
+namespace JuvoPlayer.Tests.UnitTests
+{
+    [TestFixture]
+    class TSWebVttSubtitleParser
+    {
+
+        [TestCase(null, Description = "Null time")]
+        [TestCase("", Description = "Empty time")]
+        [TestCase("0,0,0.0", Description = "Invalid separators")]
+        public void ParseTime_TimeFormatIsInvalid_ThrowsFormatException(string invalidTimeToParse)
+        {
+            var parser = CreateWebVttParser();
+
+            Assert.Throws<FormatException>(() => { parser.ParseTime(invalidTimeToParse);});
+        }
+
+        [TestCase("00:01:18.171", 0, 1, 18, 171)]
+        [TestCase("01:18.171", 0, 1, 18, 171)]
+        [TestCase("9999:01:18.171", 9999, 1, 18, 171)]
+        public void ParseTime_TimeFormatIsValid_ParsesSuccessfully(string validTimeToParse, int expectedHours, int expectedMinutes, int expectedSeconds, int expectedMilliseconds)
+        {
+            var parser = CreateWebVttParser();
+            var expectedTimeSpan =
+                new TimeSpan(0, expectedHours, expectedMinutes, expectedSeconds, expectedMilliseconds);
+
+            var parsedTimeSpan = parser.ParseTime(validTimeToParse);
+
+            Assert.That(parsedTimeSpan, Is.EqualTo(expectedTimeSpan));
+        }
+
+        private WebVttSubtitleParser CreateWebVttParser()
+        {
+            return new WebVttSubtitleParser();
+        }
+    }
+}
---
+++ JuvoPlayer/Subtitles/WebVttSubtitleParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JuvoPlayer.Subtitles
+{
+    internal class WebVttSubtitleParser : ISubtitleParser
+    {
+        public IEnumerable<Cue> Parse(StreamReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cue> Parse(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal TimeSpan ParseTime(string timeString)
+        {
+            if (string.IsNullOrEmpty(timeString))
+                throw new FormatException("Invalid time format");
+
+            timeString = timeString.Replace('.', ':');
+            string[] parts = timeString.Split(':');
+
+            if (parts.Length != 3 && parts.Length != 4)
+                throw new FormatException("Invalid time format");
+
+            int days = 0;
+            int nextPartIndex = 0;
+            int hours = parts.Length == 4 ? Int32.Parse(parts[nextPartIndex++]) : 0;
+            int minutes = Int32.Parse(parts[nextPartIndex++]);
+            int seconds = Int32.Parse(parts[nextPartIndex++]);
+            int milliseconds = Int32.Parse(parts[nextPartIndex++]);
+
+            return new TimeSpan(days, hours, minutes, seconds, milliseconds);
+        }
+    }
+}
