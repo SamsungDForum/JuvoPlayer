e8e016a0d8775a18771060a9449a2e5afebfbf53
Pawel Panek <p.panek@samsung.com>
2018-03-29 11:45:51 +0200

    Add CuesMap documentation
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

--- JuvoPlayer/Subtitles/CuesMap.cs
+++ JuvoPlayer/Subtitles/CuesMap.cs
@@ -4,12 +4,20 @@ using System.Text;
 
 namespace JuvoPlayer.Subtitles
 {
+    /// <summary>Maps a particular time offset to a corresponding cue.
+    /// Implementation is based on Binary Search Symbol Table.</summary>
     internal class CuesMap
     {
         private List<Cue> cues = new List<Cue>();
 
         public int Count => cues.Count;
 
+        /// <summary>Puts a given <see cref="T:JuvoPlayer.Subtitles.Cue"></see> in a map.
+        /// If a map doesn't contain a cue with a overlapping range (NewCue.Begin is not
+        /// in range [OldQue.Begin, OldQue.End)), then a given cue in instered in a map.
+        /// If a map contains a cue with a overlapping range, then a given cue replaces
+        /// a old cue.</summary>
+        /// <param name="cue">A cue to put in a map</param>
         public void Put(Cue cue)
         {
             int i = Rank(cue.Begin);
@@ -21,6 +29,11 @@ namespace JuvoPlayer.Subtitles
             cues.Insert(i, cue);
         }
 
+        /// <summary>Returns a <see cref="T:JuvoPlayer.Subtitles.Cue"></see> cue,
+        /// which satisfies a condition: <paramref name="value">value</paramref>
+        /// is in range of [cue.Begin, cue.End).</summary>
+        /// <param name="key">A key used to lookup for cue</param>
+        /// <returns>Found <see cref="T:JuvoPlayer.Subtitles.Cue"></see> cue if a map contains it and null if not</returns>
         public Cue Get(TimeSpan key)
         {
             if (Count == 0)
@@ -31,6 +44,9 @@ namespace JuvoPlayer.Subtitles
             return null;
         }
 
+        /// <summary>Returns a number of cues smaller than a given <see cref="T:System.TimeSpan"></see> key.</summary>
+        /// <param name="key">A key</param>
+        /// <returns>Number of cues smaller than a given key</returns>
         private int Rank(TimeSpan key)
         {
             return Rank(key, 0, cues.Count - 1);
