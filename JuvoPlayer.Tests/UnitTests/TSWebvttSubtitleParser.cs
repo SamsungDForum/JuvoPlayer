c519c557de4b2db51f2fa844d0aa4480086255df
Pawel Panek <p.panek@samsung.com>
2018-03-30 08:19:57 +0200

    WebVttSubtitleParser - ParseTimeLine
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

--- JuvoPlayer.Tests/UnitTests/TSWebvttSubtitleParser.cs
+++ JuvoPlayer.Tests/UnitTests/TSWebvttSubtitleParser.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using JuvoPlayer.Subtitles;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace JuvoPlayer.Tests.UnitTests
@@ -9,29 +11,59 @@ namespace JuvoPlayer.Tests.UnitTests
     [TestFixture]
     class TSWebVttSubtitleParser
     {
-
         [TestCase(null, Description = "Null time")]
         [TestCase("", Description = "Empty time")]
         [TestCase("0,0,0.0", Description = "Invalid separators")]
-        public void ParseTime_TimeFormatIsInvalid_ThrowsFormatException(string invalidTimeToParse)
+        [TestCase("0.0.0.0", Description = "Invalid separators")]
+        [TestCase("00:00:00:00.0", Description = "Too many elements")]
+        [TestCase("00.0", Description = "Not enough elements")]
+        public void ParseTime_TimeFormatIsInvalid_ThrowsFormatException(string invalidTime)
         {
             var parser = CreateWebVttParser();
 
-            Assert.Throws<FormatException>(() => { parser.ParseTime(invalidTimeToParse);});
+            Assert.Throws<FormatException>(() => { parser.ParseTime(invalidTime); });
         }
 
         [TestCase("00:01:18.171", 0, 1, 18, 171)]
         [TestCase("01:18.171", 0, 1, 18, 171)]
         [TestCase("9999:01:18.171", 9999, 1, 18, 171)]
-        public void ParseTime_TimeFormatIsValid_ParsesSuccessfully(string validTimeToParse, int expectedHours, int expectedMinutes, int expectedSeconds, int expectedMilliseconds)
+        public void ParseTime_TimeFormatIsValid_ParsesSuccessfully(string validTime, int hours, int minutes,
+            int seconds, int milliseconds)
         {
             var parser = CreateWebVttParser();
             var expectedTimeSpan =
-                new TimeSpan(0, expectedHours, expectedMinutes, expectedSeconds, expectedMilliseconds);
+                new TimeSpan(0, hours, minutes, seconds, milliseconds);
 
-            var parsedTimeSpan = parser.ParseTime(validTimeToParse);
+            var parsedTimeSpan = parser.ParseTime(validTime);
 
             Assert.That(parsedTimeSpan, Is.EqualTo(expectedTimeSpan));
+        }
+
+        [TestCase(null, Description = "Null line")]
+        [TestCase("", Description = "Empty line")]
+        [TestCase("01:18.171 -> 01:20.171", Description = "Invalid separator")]
+        [TestCase("01:18.171 -- 01:20.171", Description = "Invalid separator")]
+        public void ParseTimeLine_TimeLineIsInvalid_ThrowsFormatException(string invalidTimeLine)
+        {
+            var parser = CreateWebVttParser();
+
+            Assert.Throws<FormatException>(() => { parser.ParseTimeLine(invalidTimeLine); });
+        }
+
+        [TestCase("01:18.171 --> 01:20.171")]
+        [TestCase("01:18.171  -->   01:20.171")]
+        [TestCase("01:18.171 --> 01:20.171 position:10%,line-left align:left size:35%")]
+        public void ParseTimeLine_TimeLineIsValid_ParsesSuccessfully(string validTimeLine)
+        {
+            var parser = Substitute.ForPartsOf<WebVttSubtitleParser>();
+            var passTimeSpan = TimeSpan.MaxValue;
+            var validTimePattern = @"^\d+:\d+\.\d+$";
+            parser.ParseTime(Arg.Is<string>(arg => Regex.IsMatch(arg, validTimePattern))).Returns(passTimeSpan);
+
+            (var begin, var end) = parser.ParseTimeLine(validTimeLine);
+
+            Assert.That(begin, Is.EqualTo(passTimeSpan));
+            Assert.That(end, Is.EqualTo(passTimeSpan));
         }
 
         private WebVttSubtitleParser CreateWebVttParser()
--- JuvoPlayer/Subtitles/WebVttSubtitleParser.cs
+++ JuvoPlayer/Subtitles/WebVttSubtitleParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace JuvoPlayer.Subtitles
 {
@@ -17,16 +18,30 @@ namespace JuvoPlayer.Subtitles
             throw new NotImplementedException();
         }
 
-        internal TimeSpan ParseTime(string timeString)
+        internal (TimeSpan, TimeSpan) ParseTimeLine(string timeLine)
+        {
+            if (string.IsNullOrEmpty(timeLine))
+                throw new FormatException("Time line cannot be null");
+
+            string[] parts = timeLine.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+
+            const string timeSeparator = "-->";
+            if (parts.Length < 3 || !parts[1].Equals(timeSeparator))
+                throw new FormatException(string.Format("Invalid time line format [{0}]", timeLine));
+
+            return (ParseTime(parts[0]), ParseTime(parts[2]));
+        }
+
+        internal virtual TimeSpan ParseTime(string timeString)
         {
             if (string.IsNullOrEmpty(timeString))
-                throw new FormatException("Invalid time format");
+                throw new FormatException("Time string cannot be null");
+
+            if (!Regex.IsMatch(timeString, @"^\d+:\d+:\d+\.\d+$") && !Regex.IsMatch(timeString, @"^\d+:\d+\.\d+$"))
+                throw new FormatException(string.Format("Invalid time format [{0}]", timeString));
 
             timeString = timeString.Replace('.', ':');
             string[] parts = timeString.Split(':');
-
-            if (parts.Length != 3 && parts.Length != 4)
-                throw new FormatException("Invalid time format");
 
             int days = 0;
             int nextPartIndex = 0;
