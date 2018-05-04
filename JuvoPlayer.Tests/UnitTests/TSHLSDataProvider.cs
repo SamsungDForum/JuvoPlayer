3bc38daddb2165fdf981f5b01dd719ca0443fc84
Pawel Panek <p.panek@samsung.com>
2018-05-04 09:30:40 +0200

    HLSDataProvider unit tests
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

---
+++ JuvoPlayer.Tests/UnitTests/TSHLSDataProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using JuvoPlayer.Common;
+using JuvoPlayer.DataProviders.HLS;
+using JuvoPlayer.Demuxers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace JuvoPlayer.Tests.UnitTests
+{
+    [TestFixture]
+    class TSHLSDataProvider
+    {
+
+        [Test]
+        public void GetStreamsDescription_ClipWithNoSubtitles_ReturnsEmptyList()
+        {
+            var demuxerMock = Substitute.For<IDemuxer>();
+            var clipDefinition = new ClipDefinition();
+            var dataProvider = new HLSDataProvider(demuxerMock, clipDefinition);
+
+            var streamsDescription = dataProvider.GetStreamsDescription(StreamType.Subtitle);
+
+            Assert.That(streamsDescription, Is.Empty);
+        }
+
+        [Test]
+        public void OnChangeActiveStream_UnknownSubtitles_ThrowsArgumentException()
+        {
+            var demuxerMock = Substitute.For<IDemuxer>();
+            var clipDefinition = new ClipDefinition();
+            var dataProvider = new HLSDataProvider(demuxerMock, clipDefinition);
+            var streamDescription = new StreamDescription
+            {
+                StreamType = StreamType.Subtitle
+            };
+
+            Assert.Throws<ArgumentException>(() => dataProvider.OnChangeActiveStream(streamDescription));
+        }
+    }
+}
