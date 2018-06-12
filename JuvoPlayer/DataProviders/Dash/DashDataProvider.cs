4ab12b4e4fb89e0b90adcf2f8249634d879a38db
Maciej Sawicki <m.sawicki@DIGITAL.local>
2018-06-12 12:13:43 +0200

    fix Call OnStopped() SemaphoreSlim removal Call Change / unused var removal Instead of calling individual:
    videoPipeline.Stop()
    audioPipeline.Stop()
    
    during Stream Error, OnStopped() is called which does excatly same
    
    Removed SemaphoreSlim

--- JuvoPlayer/DataProviders/Dash/DashDataProvider.cs
+++ JuvoPlayer/DataProviders/Dash/DashDataProvider.cs
@@ -31,8 +31,6 @@ namespace JuvoPlayer.DataProviders.Dash
         public event PacketReady PacketReady;
         public event StreamError StreamError;
 
-        private ManualResetEventSlim waitForManifest = new ManualResetEventSlim(false);
-
         private bool disposed;
 
         public DashDataProvider(
@@ -357,12 +355,11 @@ namespace JuvoPlayer.DataProviders.Dash
             // It is possible to forgo calling stop here, simply raise StreamError event
             // and wait for termination as part of player window closure. Imho, better to call
             // quits as early as possible.
-            audioPipeline.Stop();
-            videoPipeline.Stop();
+            OnStopped();
 
             // Bubble up stream error info up to PlayerController which will shut down
             // underlying player
-            
+
             StreamError?.Invoke(errorMessage);
         }
 
