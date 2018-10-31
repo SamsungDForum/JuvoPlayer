f3ba3ae1c85acd04b175b7b88978821ea545c533
Maciej Sawicki/Multichannel Video Services (VD) /SRPOL/Staff Engineer/삼성전자 <m.sawicki@samsung.com>
2018-10-31 16:35:25 +0100

    add representation update in Dash Client Manifest update in Dash Client
    - Added representation update in DashClient when manifest changes
    - Added time based next chunk retrieval after representation update in
    Dash Client

--- JuvoPlayer/DataProviders/Dash/DashMediaPipeline.cs
+++ JuvoPlayer/DataProviders/Dash/DashMediaPipeline.cs
@@ -169,7 +169,9 @@ namespace JuvoPlayer.DataProviders.Dash
                     GetAvailableStreams(media, currentMedia);
 
                     // Media Preparation (Call to Initialize) is done upon assignment to pendingStream.
-                    PendingStream = new DashStream(currentMedia, currentRepresentation);
+                    //PendingStream = new DashStream(currentMedia, currentRepresentation);
+                    currentStream = new DashStream(currentMedia, currentRepresentation);
+                    dashClient.UpdateRepresentation(currentStream.Representation);
                     return;
                 }
             }
@@ -606,7 +608,7 @@ namespace JuvoPlayer.DataProviders.Dash
                     // Add last seek value to packet clock. Forcing last seek value looses
                     // PTS/DTS differences causing lip sync issues.
                     //
-                    demuxerClock = (PacketTimeStamp) packet + lastSeek.Value;
+                    demuxerClock = (PacketTimeStamp)packet + lastSeek.Value;
 
                     Logger.Info($"{StreamType}: Badly timestamped packet. Adjusting demuxerClock to: {demuxerClock}");
                 }
