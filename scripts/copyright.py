1821b290b567fda6de6251ebc24c65c40aea6a3f
Grzegorz Skowinski <g.skowinski@samsung.com>
2018-12-07 11:20:10 +0100

    Modified script, added missing copyright noticies.

--- scripts/copyright.py
+++ scripts/copyright.py
@@ -1,15 +1,14 @@
-import os
-import re
+import os, re
 
 def readBlacklist(path):
     with open(path, 'r') as f:
-        data = f.readlines()
-        return data
+        lines = [l for l in (line.strip() for line in f) if l]
+        return lines
 
 def readCopyright(path):
     with open(path, 'r') as f:
         data = f.read()
-        return data
+        return data.strip()
 
 def writeCopyright(path, copyright):
     with open(path, 'r+') as f:
@@ -17,26 +16,30 @@ def writeCopyright(path, copyright):
         f.seek(0)
         f.write(copyright + "\n" + data)
 
-def containsCopyright(data, copyright):
-    return None != re.match("\s*" + re.escape(copyright), data)
+def containsCopyright(data, regex):
+    return regex.match(data) != None
 
-def processFile(path, copyright):
+def processFile(path, copyright, regex):
     with open(path) as open_file:
         data = open_file.read()
-        if not containsCopyright(data, copyright):
+        if not containsCopyright(data, regex):
             print("[+] %s" % path)
             writeCopyright(path, copyright)
         else:
             print("[ ] %s" % path)
 
+def prepareRegex(copyright):
+    return re.compile("\s*" + re.sub(r'\n', '\r?\n', re.escape(copyright)), re.MULTILINE)
+
 def isApplicable(path, blacklist):
-    return fname.endswith(".cs") and all(not fname.endswith(extension) for extension in blacklist) and "thirdparty/" not in path
+    return path.endswith(".cs") and all(not path.endswith(extension) for extension in blacklist) and "thirdparty/" not in path
 
 def processDir(rootDir, copyright, blacklist):
+    regex = prepareRegex(copyright)
     for dirName, subdirList, fileList in os.walk(rootDir):
         for fname in fileList:
             if isApplicable(dirName + '/' + fname, blacklist):
-                processFile(dirName + '/' + fname, copyright)
+                processFile(dirName + '/' + fname, copyright, regex)
 
 if __name__ == "__main__":
     processDir('..', readCopyright("copyright_notice.txt"), readBlacklist("extension_blacklist.txt"))
