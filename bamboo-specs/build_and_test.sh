e78dd51d5c28486cf9122b91f81aa5a20c38b470
Pawel Panek <p.panek@samsung.com>
2019-04-05 12:34:02 +0200

    Add Bamboo build certs
    Relates to PRJLTVA-340
    
    Signed-off-by: Pawel Panek <p.panek@samsung.com>

--- bamboo-specs/build_and_test.sh
+++ bamboo-specs/build_and_test.sh
@@ -6,8 +6,15 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
-ACTIVE_PROFILE=`tizen security-profiles list | grep "O.*$" | awk -F "[ ]+" '{ print $1 }'`
-try tizen build-cs -C Release -s ${ACTIVE_PROFILE}
+SCRIPT=`realpath $0`
+SCRIPT_PATH=`dirname ${SCRIPT}`
+CERTS_PATH="${SCRIPT_PATH}"/certs
+
+echo "SCRIPT=${SCRIPT}"
+echo "SCRIPT_PATH=${SCRIPT_PATH}"
+echo "CERTS_PATH=${CERTS_PATH}"
+
+try dotnet build /p:"AuthorPath=${CERTS_PATH}/partner_2019.p12;AuthorPass=${bamboo_AuthorPassword}" /p:"DistributorPath=${CERTS_PATH}/tizen-distributor-signer.p12;DistributorPass=${bamboo_DistributorPassword}"
 
 try dotnet test JuvoPlayer.Tests/JuvoPlayer.Tests.csproj --logger:trx -f netcoreapp2.0
 
