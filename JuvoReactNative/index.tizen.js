dfebbc39321a59d2f5aaea5d3e3ecc922c7985c3
p.buczynski <p.buczynski@samsung.com>
2019-07-19 13:11:05 +0200

    HOT module reloading mechanism adoption. HidableView component added

--- JuvoReactNative/index.tizen.js
+++ JuvoReactNative/index.tizen.js
@@ -4,107 +4,57 @@
  * @flow
  */
 'use strict'
-
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
+  StyleSheet,  
+  View,
+  Text,
   ScrollView,
   Image,
-  View,
-  TouchableOpacity,
-  Text,
-  StyleSheet,
-  AppRegistry,
-  Alert,
-  Button
+  Alert,  
+  Button, 
+  Animated,
+  AppRegistry
 } from 'react-native';
 
-//var NativeEventEmitter = require('NativeEventEmitter');
+
+
+const deviceWidth = 298 //Dimensions.get('window').width / 100
+const FIXED_BAR_WIDTH = 1
+const BAR_SPACE = 1
+
 import { NativeModules } from 'react-native';
 const JuvoPlayer = NativeModules.JuvoPlayer;
-//var JuvoPlayerEventEmitter = new NativeEventEmitter(JuvoPlayer);
-var ProgressBar = require('ProgressBarAndroid');
-var TimerMixin = require('react-timer-mixin');
+
+const PlayVideo = (name) => {
+  try {
+    Alert.alert('Ok.' + name);
+    //JuvoPlayer.startPlayback();    
+
+  } catch (e) {
+    Alert.alert('Error! ' + e);
+  }
+};
 
 class Thumb extends React.Component {
-
   shouldComponentUpdate(nextProps, nextState) {
     return false;
   }
 
   render() {
     return (
-        <View style={styles.thumb}>
-          <TouchableOpacity
-          onPress={PlayVideo}>
-            <Image style={styles.img} source={{uri: this.props.source}} />
-          </TouchableOpacity>
-        </View>
-    );
-  }
-
-}
-
-class VerticalScrollView extends React.Component {
-  //title: '<ScrollView>',
-  //description: 'To make content scrollable, wrap it within a <ScrollView> component',
-  render () {
-    var _scrollView;
-    return (
-      <View>
-        <ScrollView
-          ref={(scrollView) => { _scrollView = scrollView; }}
-          automaticallyAdjustContentInsets={false}
-          onScroll={() => { console.log('onScroll!'); }}
-          scrollEventThrottle={200}
-          style={styles.scrollView}>
-          {THUMB_URIS.map(createThumbRow)}
-        </ScrollView>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => { _scrollView.scrollTo({y: 0}); }}>
-            <Text style = {{color:'black'}}>Scroll to top</Text>
-        </TouchableOpacity>
-          <TouchableOpacity
-          style={styles.button}
-          onPress={() => { _scrollView.scrollToEnd({animated: true}); }}>
-            <Text style = {{color:'black'}}>Scroll to bottom</Text>
-        </TouchableOpacity>
+      <View style={styles.thumb}>
+        <Button style={styles.button_thumb}  
+          title = ''                            
+          onPress = {PlayVideo}
+          name={this.props.source}
+          >                       
+        </Button>       
+        <Image style={styles.img} source={{uri: this.props.source}} />
       </View>
     );
   }
 }
-
-class HorizontalScrollView extends React.Component {
-  //title: '<ScrollView> (horizontal = true)',
-  //description: 'You can display <ScrollView>\'s child components horizontally rather than vertically',
-  render() {
-    var _scrollView;
-    var addtionalStyles = {direction: 'ltr'};
-    return (
-        <View style={addtionalStyles}>
-          <ScrollView
-            ref={(scrollView) => { _scrollView = scrollView; }}
-            automaticallyAdjustContentInsets={false}
-            scrollEventThrottle={100}
-            horizontal={true}
-            style={[styles.scrollView, styles.horizontalScrollView]}>
-            {THUMB_URIS.map(createThumbRow)}
-          </ScrollView>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => { _scrollView.scrollTo({x: 0}); }}>
-             <Text style = {{color:'black'}}>Scroll to start</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => { _scrollView.scrollToEnd({animated: true}); }}>
-              <Text style = {{color:'black'}}>Scroll to end</Text>
-          </TouchableOpacity>
-        </View>
-      );
-  }
-}
-
 var THUMB_URIS = [
   'https://github.com/SamsungDForum/JuvoPlayer/blob/master/smarthubpreview/pictures/car.jpg?raw=true',
   'https://github.com/SamsungDForum/JuvoPlayer/blob/master/smarthubpreview/pictures/car.jpg?raw=true',
@@ -117,102 +67,236 @@ var THUMB_URIS = [
 ];
 var createThumbRow = (uri, i) => <Thumb key={i} source={uri} />;
 
-var MovingBar = React.createClass({
-  mixins: [TimerMixin],
-
-  getInitialState: function() {
-    return {
-      progress: 0
+class HorizontalScrollView extends React.Component { 
+  render() {
+    var _scrollView;    
+    var curIndex = 0
+    var numItems = THUMB_URIS.length
+    var itemWidth = (FIXED_BAR_WIDTH / numItems) - ((numItems - 1) * BAR_SPACE)
+    var animVal = new Animated.Value(0)
+  
+    var _handleButtonPressRight = () => {     
+    if (curIndex < numItems - 1){
+        curIndex++;        
+       _scrollView.scrollTo({x: curIndex*deviceWidth, y: 0, animated: true})        
+      }
     };
-  },
 
-  componentDidMount: function() {
-    this.setInterval(
-      () => {
-        var progress = (this.state.progress + 0.02) % 1;
-        this.setState({progress: progress});
-      }, 50
-    );
-  },
+    var _handleButtonPressLeft = () => {      
+     if (curIndex > 0){
+         curIndex--;       
+        _scrollView.scrollTo({x: curIndex*deviceWidth, y: 0, animated: true})       
+     }
+    };
 
-  render: function() {
-    return <ProgressBar progress={this.state.progress} {...this.props} />;
-  },
-});
-
-const PlayVideo = () => {
-  try {
-    JuvoPlayer.startPlayback();    
-  } catch (e) {
-    Alert.alert('Error! ' + e);
+    return (
+        <View style = {styles.horizontalScrollView}>
+          <View style = {styles.control_buttons}>
+            <Button
+              title="Scroll to left"
+              accessibilityLabel="Learn more about this  button"           
+              onPress={ _handleButtonPressLeft }>             
+            </Button>
+            <Button
+              title="Scroll to start"
+              accessibilityLabel="Learn more about this  button"              
+              onPress={() => { _scrollView.scrollTo({x: 0}); }}>             
+            </Button>
+            <Button
+              title="Scroll to right"
+              accessibilityLabel="Learn more about this  button"            
+              onPress={ _handleButtonPressRight }>             
+            </Button>          
+          </View>          
+          <ScrollView
+            ref={(scrollView) => { _scrollView = scrollView; }}
+            automaticallyAdjustContentInsets={false}
+            scrollEventThrottle={100}
+            horizontal={true}
+            showsHorizontalScrollIndicator = {false}             
+            style={[styles.scrollView]}>
+            {THUMB_URIS.map(createThumbRow)}
+          </ScrollView>
+        </View>
+      );
   }
-};
+}
 
-export default class JuvoReactNative extends Component {  
+
+export default class JuvoReactNative extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+        visible: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+      this.setState({
+          visible: !this.state.visible
+      });
+  }
+
   render() {
     return (
-      <View style={{width:1920, height: 1080}}>
-        <Button style={{width:300, height: 100}}
-                onPress={PlayVideo}
-                title="Start!"
-                accessibilityLabel="See an informative alert"
-        />
-        <Image source={{uri: 'https://github.com/SamsungDForum/JuvoPlayer/blob/master/smarthubpreview/pictures/car.jpg?raw=true', width: 208, height: 180}} />
-        <Image source={{uri: '/home/owner/apps_rw/JuvoReactNative/res/tiles/bolid.jpg', width: 208, height: 180}} />
-        <MovingBar horizontal={true} style={{top:650, width:1860, height:40, backgroundColor: 'green', color: 'blue'}} />
-        <HorizontalScrollView />
+      <View style={styles.container}>       
+       <Image style={styles.img_big} source={{uri: 'https://github.com/SamsungDForum/JuvoPlayer/blob/master/smarthubpreview/pictures/car.jpg?raw=true'}} />
+      
+        <HideableView 
+          visible={this.state.visible} 
+          style = {styles.clip_details}>
+          <Text style = {styles.clip_details_text}>
+            Hello !
+          </Text>                  
+        </HideableView >  
+        <Button style = {{width: '100%', backgroundColor: 'transparent'}}
+            onPress={this.toggle}
+            title="Start video!"            
+            accessibilityLabel="See an informative alert"
+          />        
+          <HideableView 
+            visible = {this.state.visible}>
+              <HorizontalScrollView/>
+          </HideableView>                
       </View>
     );
   }
 }
 
+
+class HideableView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opacity: new Animated.Value(this.props.visible ? 1 : 0)
+    }
+  }
+
+  animate(show) {
+    const duration = this.props.duration ? parseInt(this.props.duration) : 500;
+    Animated.timing(
+      this.state.opacity, {
+        toValue: show ? 1 : 0,
+        duration: !this.props.noAnimation ? duration : 0
+      }
+    ).start();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.visible !== nextProps.visible;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (this.props.visible !== nextProps.visible) {
+      this.animate(nextProps.visible);
+    }
+  }
+
+  render() {
+    if (this.props.removeWhenHidden) {
+      return (this.visible && this.props.children);
+    }
+    return (
+      <Animated.View style={{opacity: this.state.opacity}}>
+        {this.props.children}
+      </Animated.View>
+    )
+  }
+}
+
+HideableView.propTypes = {
+  visible: PropTypes.bool.isRequired,
+  duration: PropTypes.number,
+  removeWhenHidden: PropTypes.bool,
+  noAnimation: PropTypes.bool
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',    
-    backgroundColor: "transparent",
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',    
+    backgroundColor: "transparent",   
+    width: '100%', 
+    height: '100%'
+  },   
   scrollView: {
-    backgroundColor: '#eeeeee',
-    height: 350,
+    height: '25%',
+    width: 1920,
   },
   horizontalScrollView: {
-    height: 280,
-    width: 1920,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
   },
   text: {
     fontSize: 16,
     fontWeight: 'bold',
     margin: 5,
-  },
-  button: {
-    margin: 5,
-    padding: 5,
+  },  
+  button_thumb: {
+    position: 'absolute',
     alignItems: 'center',
-    backgroundColor: '#cccccc',
-    borderRadius: 3,
+    backgroundColor:  '#00000070',       
+    width: '100%',
+    height: '100%'
   },
   thumb: {
-    margin: 5,
-    padding: 5,
-    backgroundColor: '#cccccc',
-    borderRadius: 3,
-    minWidth: 96,    
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',        
+    backgroundColor: '#cccccc',    
+    width: 459,        
+    height: '98%'
   },
-  img: {
-    width: 460,
-    height: 264,
+  img: {     
+    justifyContent: 'center', 
+    alignItems: 'center',     
+    width: '96%',
+    height: '96%', 
+   },
+   img_big: {    
+   position : 'absolute', 
+   justifyContent: 'flex-start', 
+   alignItems: 'flex-start',     
+   width: '100%',
+   height: '100%', 
+  },
+  img_big_hide: {    
+    position : 'absolute', 
+    justifyContent: 'flex-start', 
+    alignItems: 'flex-start',     
+    width: '10%',
+    height: '10%', 
+    backgroundColor: "transparent"  
+   },
+  control_buttons : {
+     justifyContent: 'space-between',
+     alignItems: 'flex-start',
+     flexDirection: 'row',
+     alignItems: 'stretch'
+  },
+  clip_details: {
+    flex: 2,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+     flexDirection: 'column',
+     alignItems: 'stretch',
+     width: '100%',
+     height: '100%'
+  },
+  clip_details_text: {    
+    width: '100%',
+    height: '89%',
+    backgroundColor: '#345636',
+    fontSize: 50,
+    textAlign: 'right',
+    textAlignVertical: 'center'
   }
 });
 
