a24eb67424eff9a77a47e458316357ce7b670d80
p.buczynski <p.buczynski@samsung.com>
2019-08-13 10:23:27 +0200

    loacal images moved to the subfolder

--- JuvoReactNative/index.tizen.js
+++ JuvoReactNative/index.tizen.js
@@ -6,13 +6,13 @@
 'use strict'
 import React, { Component, PropTypes } from 'react';
 import {
-  StyleSheet,  
+  StyleSheet,
   View,
   Text,
   ScrollView,
   Image,
-  Alert,  
-  Button, 
+  Alert,
+  Button,
   Animated,
   AppRegistry
 } from 'react-native';
@@ -44,13 +44,13 @@ class Thumb extends React.Component {
   render() {
     return (
       <View style={styles.thumb}>
-        <Button style={styles.button_thumb}  
-          title = ''                            
-          onPress = {() => PlayVideo(this.props.source)}
+        <Button style={styles.button_thumb}
+          title=''
+          onPress={() => PlayVideo(this.props.source)}
           name={this.props.source}
-          >                       
-        </Button>       
-        <Image style={styles.img} source={{uri: this.props.source}} />
+        >
+        </Button>
+        <Image style={styles.img} source={{ uri: this.props.source }} />
       </View>
     );
   }
@@ -67,101 +67,101 @@ var THUMB_URIS = [
 ];
 var createThumbRow = (uri, i) => <Thumb key={i} source={uri} />;
 
-class HorizontalScrollView extends React.Component { 
+class HorizontalScrollView extends React.Component {
   render() {
-    var _scrollView;    
+    var _scrollView;
     var curIndex = 0
     var numItems = THUMB_URIS.length
     var itemWidth = (FIXED_BAR_WIDTH / numItems) - ((numItems - 1) * BAR_SPACE)
     var animVal = new Animated.Value(0)
-  
-    var _handleButtonPressRight = () => {     
-    if (curIndex < numItems - 1){
-        curIndex++;        
-       _scrollView.scrollTo({x: curIndex*deviceWidth, y: 0, animated: true})        
+
+    var _handleButtonPressRight = () => {
+      if (curIndex < numItems - 1) {
+        curIndex++;
+        _scrollView.scrollTo({ x: curIndex * deviceWidth, y: 0, animated: true })
       }
     };
-    var _handleButtonPressLeft = () => {      
-     if (curIndex > 0){
-         curIndex--;       
-        _scrollView.scrollTo({x: curIndex*deviceWidth, y: 0, animated: true})       
-     }
+    var _handleButtonPressLeft = () => {
+      if (curIndex > 0) {
+        curIndex--;
+        _scrollView.scrollTo({ x: curIndex * deviceWidth, y: 0, animated: true })
+      }
     };
 
     return (
-        <View style = {styles.horizontalScrollView}>
-           <ScrollView
-            ref={(scrollView) => { _scrollView = scrollView; }}
-            automaticallyAdjustContentInsets={false}
-            scrollEventThrottle={100}
-            horizontal={true}
-            showsHorizontalScrollIndicator = {false}             
-            style={[styles.scrollView]}>
-            {THUMB_URIS.map(createThumbRow)}
-          </ScrollView>
-          <View style = {styles.control_buttons}>
-            <Button
-              title="Scroll to left"
-              accessibilityLabel="Learn more abo ut this  button"           
-              onPress={ _handleButtonPressLeft }>             
-            </Button>
-            <Button
-              title="Scroll to start"
-              accessibilityLabel="Learn more about this  button"              
-              onPress={() => { _scrollView.scrollTo({x: 0}); }}>             
-            </Button>
-            <Button
-              title="Scroll to right"
-              accessibilityLabel="Learn more about this  button"            
-              onPress={ _handleButtonPressRight }>             
-            </Button>          
-          </View>          
+      <View style={styles.horizontalScrollView}>
+        <ScrollView
+          ref={(scrollView) => { _scrollView = scrollView; }}
+          automaticallyAdjustContentInsets={false}
+          scrollEventThrottle={100}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          style={[styles.scrollView]}>
+          {THUMB_URIS.map(createThumbRow)}
+        </ScrollView>
+        <View style={styles.control_buttons}>
+          <Button
+            title="Scroll to left"
+            accessibilityLabel="Learn more abo ut this  button"
+            onPress={_handleButtonPressLeft}>
+          </Button>
+          <Button
+            title="Scroll to start"
+            accessibilityLabel="Learn more about this  button"
+            onPress={() => { _scrollView.scrollTo({ x: 0 }); }}>
+          </Button>
+          <Button
+            title="Scroll to right"
+            accessibilityLabel="Learn more about this  button"
+            onPress={_handleButtonPressRight}>
+          </Button>
         </View>
-      );
+      </View>
+    );
   }
 }
 
 export default class JuvoReactNative extends Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
-        visible: false
+      visible: false
     };
     this.toggle = this.toggle.bind(this);
   }
 
   toggle() {
-      this.setState({
-          visible: !this.state.visible
-      });
+    this.setState({
+      visible: !this.state.visible
+    });
+    if (this.state.visible)
+      PlayVideo('url');
   }
+
+
 
   render() {
     return (
-      <View style={styles.container}>  
-        <HideableView 
-          visible={true}>           
-          <Image 
-            style={styles.img_big} 
-            source={{uri: 'https://github.com/SamsungDForum/JuvoPlayer/blob/master/smarthubpreview/pictures/car.jpg?raw=true'}} />
-        </HideableView>
-        <HideableView 
-          visible={this.state.visible} 
-          style = {styles.clip_details}>
-          <Text style = {styles.clip_details_text}>
+
+      <View style={styles.container}>
+        <HideableView
+          visible={this.state.visible}
+          style={styles.clip_details}>
+          <Image style={styles.img_big} source={require('./res/images/car.png')} />
+          <Text style={styles.clip_details_text}>
             Hello !
-          </Text>                  
-        </HideableView >  
-        <Button style = {{width: '100%', backgroundColor: 'transparent'}}
-            onPress={this.toggle}
-            title="Start video!"            
-            accessibilityLabel="See an informative alert"
-          />        
-          <HideableView 
-            visible = {this.state.visible}>
-              <HorizontalScrollView/>
-          </HideableView>                
+          </Text>
+        </HideableView >
+        <Button style={{ width: '100%', backgroundColor: 'transparent' }}
+          onPress={this.toggle}
+          title="Start video!"
+          accessibilityLabel="See an informative alert"
+        />
+        <HideableView
+          visible={this.state.visible}>
+
+        </HideableView>
       </View>
     );
   }
@@ -201,7 +201,7 @@ class HideableView extends Component {
       return (this.visible && this.props.children);
     }
     return (
-      <Animated.View style={{opacity: this.state.opacity}}>
+      <Animated.View style={{ opacity: this.state.opacity }}>
         {this.props.children}
       </Animated.View>
     )
@@ -221,11 +221,11 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-end',
-    alignItems: 'flex-end',    
-    backgroundColor: "transparent",   
-    width: '100%', 
+    alignItems: 'flex-end',
+    backgroundColor: "transparent",
+    width: '100%',
     height: '100%'
-  },   
+  },
   scrollView: {
     height: '46%',
     width: 1920,
@@ -239,11 +239,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     margin: 5,
-  },  
+  },
   button_thumb: {
     position: 'absolute',
     alignItems: 'center',
-    backgroundColor:  '#00000070',       
+    backgroundColor: '#00000070',
     width: '100%',
     height: '100%'
   },
@@ -251,50 +251,48 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',        
-    backgroundColor: '#cccccc',    
-    width: 459,        
+    alignItems: 'center',
+    backgroundColor: '#cccccc',
+    width: 459,
     height: 260
   },
-  img: {     
-    justifyContent: 'center', 
-    alignItems: 'center',     
+  img: {
+    justifyContent: 'center',
+    alignItems: 'center',
     width: '96%',
-    height: '96%', 
-   },
-   img_big: {    
-   position : 'absolute', 
-   justifyContent: 'flex-start', 
-   alignItems: 'flex-end',     
-   width: '100%',
-   height: '100%', 
+    height: '96%',
   },
-  img_big_hide: {    
-    position : 'absolute', 
-    justifyContent: 'flex-start', 
-    alignItems: 'flex-start',     
+  img_big: {
+    width: '100%',
+    height: '100%',
+    position: "absolute",
+    top: 0,
+    left: 0
+  },
+  img_big_hide: {
+    position: 'absolute',
     width: '10%',
-    height: '10%', 
-    backgroundColor: "transparent"  
-   },
-  control_buttons : {
-     justifyContent: 'space-between',
-     alignItems: 'flex-end',
-     flexDirection: 'row',
-     alignItems: 'stretch'
+    height: '10%',
+    backgroundColor: "transparent",
+    top: 0,
+    left: 0
+  },
+  control_buttons: {
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    alignItems: 'stretch'
   },
   clip_details: {
-    flex: 2,
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-     flexDirection: 'column',
-     alignItems: 'stretch',
-     width: '100%',
-     height: '100%'
-  },
-  clip_details_text: {    
+    position: "absolute",
+    top: 0,
+    left: 0,
     width: '100%',
-    height: '89%',
+    height: '100%'
+  },
+  clip_details_text: {
+    width: 1920,
+    height: 1080,
     backgroundColor: '#345636',
     fontSize: 50,
     textAlign: 'right',
