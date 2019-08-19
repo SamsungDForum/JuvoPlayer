e31d7cb189a39d4475fae1ca6f935f15b1522f7d
p.buczynski <p.buczynski@samsung.com>
2019-08-19 15:18:50 +0200

    ScrollView controlled by the TV keys API

--- JuvoReactNative/index.tizen.js
+++ JuvoReactNative/index.tizen.js
@@ -38,20 +38,27 @@ const PlayVideo = (clip_url) => {
   }
 };
 
+
+
 class Thumb extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return false;
+  
+  constructor(props) {
+    super(props);  
+    this.state = {curIndex: props.curIndex}; 
+
   }
 
-  render() {
+  shouldComponentUpdate(nextProps, nextState) {
+    return true;
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {    
     return (
-      <View style={styles.thumb}>
-        <Button style={styles.button_thumb}
-          title=''
-          onPress={() => PlayVideo(this.props.source)}
-          name={this.props.source}
-        >
-        </Button>
+      <View style={styles.thumb}>        
         <Image style={styles.img} source={{ uri: this.props.source }} />
       </View>
     );
@@ -70,54 +77,93 @@ var THUMB_URIS = [
 var createThumbRow = (uri, i) => <Thumb key={i} source={uri} />;
 
 class HorizontalScrollView extends React.Component {
-  render() {
-    var _scrollView;
-    var curIndex = 0
-    var numItems = THUMB_URIS.length
-    var itemWidth = (FIXED_BAR_WIDTH / numItems) - ((numItems - 1) * BAR_SPACE)
-    var animVal = new Animated.Value(0)
 
-    var _handleButtonPressRight = () => {
-      if (curIndex < numItems - 1) {
-        curIndex++;
-        _scrollView.scrollTo({ x: curIndex * deviceWidth, y: 0, animated: true })
-      }
-    };
-    var _handleButtonPressLeft = () => {
-      if (curIndex > 0) {
-        curIndex--;
-        _scrollView.scrollTo({ x: curIndex * deviceWidth, y: 0, animated: true })
-      }
-    };
+  constructor(props) {
+    super(props);
+    this.onTVKey = this.onTVKey.bind(this);
+    this._scrollView;    
+    this.scroll_inprogress = false;    
+    this.curIndex = 0
+    this.numItems = THUMB_URIS.length
+    this.itemWidth = (FIXED_BAR_WIDTH / this.numItems) - ((this.numItems - 1) * BAR_SPACE)    
+    this._handleButtonPressRight = this._handleButtonPressRight.bind(this);
+    this._handleButtonPressLeft = this._handleButtonPressLeft.bind(this);    
+  }
 
+  _handleButtonPressRight = () => {
+    JuvoPlayer.log("curIndex = " + this.curIndex);
+    JuvoPlayer.log("numItems = " + this.numItems);
+    this.scroll_inprogress = false;    
+    if (this.curIndex < this.numItems - 1) {      
+      this.curIndex++;
+      this._scrollView.scrollTo({ x: this.curIndex * deviceWidth, y: 0, animated: true });
+    }    
+  };
+
+  _handleButtonPressLeft = () => {
+    JuvoPlayer.log("curIndex = " + this.curIndex);
+    JuvoPlayer.log("numItems = " + this.numItems);
+    this.scroll_inprogress = false;
+    if (this.curIndex > 0) {     
+      this.curIndex--;
+      this._scrollView.scrollTo({ x: this.curIndex * deviceWidth, y: 0, animated: true });  
+    };
+  };
+
+  //onMomentumScrollEnd
+  _onScroll = (event) => { 
+    if (!this.scroll_inprogress) {
+      JuvoPlayer.log("_onScroll... index = " + this.curIndex);
+      this.scroll_inprogress = true;
+    }
+    
+    //this.yOffset = event.nativeEvent.contentOffset.y;
+  };  
+
+  componentWillMount() {    
+    JuvoEventEmitter.addListener(
+      'onTVKeyPress', 
+      this.onTVKey
+    );
+  }
+
+  componentDidMount() {
+
+  }
+
+  onTVKey(pressed) {
+    //There are two parameters available:
+    //params.KeyName
+    //params.KeyCode   
+    JuvoPlayer.log("ScrollView clicked...");
+    switch (pressed.KeyName)
+      {
+        case "Right":
+            JuvoPlayer.log("Right clicked...");
+            this._handleButtonPressRight();
+            break;
+        case "Left":
+          JuvoPlayer.log("Left clicked...");
+          this._handleButtonPressLeft();
+            break;        
+      }  
+    JuvoPlayer.log("hello from Tizen world! params - KeyName  " + pressed.KeyName + " the code: " + pressed.KeyCode);
+  };  
+
+  render() {    
     return (
       <View style={styles.horizontalScrollView}>
-        <ScrollView
-          ref={(scrollView) => { _scrollView = scrollView; }}
+        <ScrollView          
+          scrollEnabled = {false}
+          ref={(scrollView) => {this._scrollView = scrollView;}}
+                  
           automaticallyAdjustContentInsets={false}
-          scrollEventThrottle={100}
+          scrollEventThrottle={16}
           horizontal={true}
           showsHorizontalScrollIndicator={false}
           style={[styles.scrollView]}>
           {THUMB_URIS.map(createThumbRow)}
-        </ScrollView>
-        <View style={styles.control_buttons}>
-          <Button
-            title="Scroll to left"
-            accessibilityLabel="Learn more abo ut this  button"
-            onPress={_handleButtonPressLeft}>
-          </Button>
-          <Button
-            title="Scroll to start"
-            accessibilityLabel="Learn more about this  button"
-            onPress={() => { _scrollView.scrollTo({ x: 0 }); }}>
-          </Button>
-          <Button
-            title="Scroll to right"
-            accessibilityLabel="Learn more about this  button"
-            onPress={_handleButtonPressRight}>
-          </Button>
-        </View>
+        </ScrollView>        
       </View>
     );
   }
@@ -158,18 +204,18 @@ export default class JuvoReactNative extends Component {
         case "XF86AudioPlay":     
             JuvoPlayer.log("Start playback...");
             if (this.state.visible) {
-              JuvoPlayer.startPlayback();
+              //JuvoPlayer.startPlayback();
               this.toggle();
             }
             else {
               //pause
-              JuvoPlayer.pauseResumePlayback();
+              //JuvoPlayer.pauseResumePlayback();
             }
             break;
         case "XF86Back":
         case "XF86AudioStop":
             if (!this.state.visible) {            
-              JuvoPlayer.stopPlayback();
+              //JuvoPlayer.stopPlayback();
               this.toggle();
             }  
             break;        
@@ -187,11 +233,10 @@ export default class JuvoReactNative extends Component {
           <Text style={styles.clip_details_text}>
             Hello world!
           </Text>
-        </HideableView >
-        
+          <HorizontalScrollView/>
+        </HideableView >        
         <HideableView
           visible={this.state.visible}>
-
         </HideableView>
       </View>
     );
@@ -259,6 +304,7 @@ const styles = StyleSheet.create({
     width: 1920,
   },
   horizontalScrollView: {
+    flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
@@ -290,12 +336,11 @@ const styles = StyleSheet.create({
     width: '96%',
     height: '96%',
   },
-  img_big: {
-    width: '100%',
-    height: '100%',
+  img_big: {    
     position: "absolute",
     top: 0,
     left: 0
+    
   },
   img_big_hide: {
     position: 'absolute',
@@ -318,9 +363,9 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%'
   },
-  clip_details_text: {
-    width: 1920,
-    height: 1080,
+  clip_details_text: {    
+    width: '60%',
+    height: '60%',
     backgroundColor: '#345636',
     fontSize: 50,
     textAlign: 'right',
