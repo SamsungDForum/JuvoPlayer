d079c04dcb9026d2fbf26a38930d08c45a04935c
p.buczynski <p.buczynski@samsung.com>
2019-08-21 14:56:09 +0200

    CSS styles cleanning. HidableView issue resolved.

--- JuvoReactNative/index.tizen.js
+++ JuvoReactNative/index.tizen.js
@@ -39,31 +39,31 @@ const PlayVideo = (clip_url) => {
 
 
 class Thumb extends React.Component {
-  
+
   constructor(props) {
-    super(props);      
+    super(props);
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     return true;
-  }  
+  }
 
-  render() {  
+  render() {
     //JuvoPlayer.log("thumb props.selectedURI " + this.props.selectedIndex);
     //JuvoPlayer.log("thumb props.myURI = " + this.props.myIndex);  
-    if ( this.props.selectedIndex == this.props.myIndex) {
+    if (this.props.selectedIndex == this.props.myIndex) {
       return (
-        <View style={styles.thumb_selected}>              
+        <View style={styles.thumb_selected}>
           <Image style={styles.img_thumb} source={{ uri: this.props.source }} />
         </View>
       );
     } else {
       return (
-        <View style={styles.thumb}>              
+        <View style={styles.thumb}>
           <Image style={styles.img_thumb} source={{ uri: this.props.source }} />
         </View>
-      );  
-    }    
+      );
+    }
   }
 }
 var THUMB_URIS = [
@@ -82,83 +82,81 @@ class HorizontalScrollView extends React.Component {
 
   constructor(props) {
     super(props);
-      
-    this._scrollView;       
-    this.curIndex = 0    
-    this.state = {selectedIndex: 0};
+
+    this._scrollView;
+    this.curIndex = 0
+    this.state = { selectedIndex: 0 };
     this.numItems = THUMB_URIS.length;
     this.itemWidth = (FIXED_BAR_WIDTH / this.numItems) - ((this.numItems - 1) * BAR_SPACE);
 
-    this.onTVKey = this.onTVKey.bind(this);    
+    this.onTVKey = this.onTVKey.bind(this);
     this._handleButtonPressRight = this._handleButtonPressRight.bind(this);
-    this._handleButtonPressLeft = this._handleButtonPressLeft.bind(this);    
+    this._handleButtonPressLeft = this._handleButtonPressLeft.bind(this);
   }
 
   _handleButtonPressRight = () => {
-   // JuvoPlayer.log("curIndex = " + this.curIndex);
-   // JuvoPlayer.log("numItems = " + this.numItems);
-    this.scroll_inprogress = false;    
-    if (this.curIndex < this.numItems - 1) {      
+    // JuvoPlayer.log("curIndex = " + this.curIndex);
+    // JuvoPlayer.log("numItems = " + this.numItems);
+    this.scroll_inprogress = false;
+    if (this.curIndex < this.numItems - 1) {
       this.curIndex++;
       this._scrollView.scrollTo({ x: this.curIndex * deviceWidth, y: 0, animated: true });
-    }    
-    this.setState({selectedIndex: this.curIndex });
+    }
+    this.setState({ selectedIndex: this.curIndex });
     this.props.onSelectedIndexChange(this.curIndex);
   };
 
   _handleButtonPressLeft = () => {
-   // JuvoPlayer.log("curIndex = " + this.curIndex);
-   // JuvoPlayer.log("numItems = " + this.numItems);
+    // JuvoPlayer.log("curIndex = " + this.curIndex);
+    // JuvoPlayer.log("numItems = " + this.numItems);
     this.scroll_inprogress = false;
-    if (this.curIndex > 0) {     
+    if (this.curIndex > 0) {
       this.curIndex--;
-      this._scrollView.scrollTo({ x: this.curIndex * deviceWidth, y: 0, animated: true });  
+      this._scrollView.scrollTo({ x: this.curIndex * deviceWidth, y: 0, animated: true });
     };
-    this.setState({selectedIndex: this.curIndex});
+    this.setState({ selectedIndex: this.curIndex });
     this.props.onSelectedIndexChange(this.curIndex);
   };
-  
-  componentWillMount() {    
+
+  componentWillMount() {
     JuvoEventEmitter.addListener(
-      'onTVKeyPress', 
+      'onTVKeyPress',
       this.onTVKey
     );
   }
-  
+
   onTVKey(pressed) {
     //There are two parameters available:
     //params.KeyName
     //params.KeyCode   
-   // JuvoPlayer.log("ScrollView clicked...");
-    switch (pressed.KeyName)
-      {
-        case "Right":
-            //JuvoPlayer.log("Right clicked...");
-            this._handleButtonPressRight();
-            break;
-        case "Left":
-         // JuvoPlayer.log("Left clicked...");
-          this._handleButtonPressLeft();
-            break;        
-      }  
-   // JuvoPlayer.log("HorizontalScrollView params - KeyName  " + pressed.KeyName + " the code: " + pressed.KeyCode);
-  };  
-   
-  render() {    
+    // JuvoPlayer.log("ScrollView clicked...");
+    switch (pressed.KeyName) {
+      case "Right":
+        //JuvoPlayer.log("Right clicked...");
+        this._handleButtonPressRight();
+        break;
+      case "Left":
+        // JuvoPlayer.log("Left clicked...");
+        this._handleButtonPressLeft();
+        break;
+    }
+    // JuvoPlayer.log("HorizontalScrollView params - KeyName  " + pressed.KeyName + " the code: " + pressed.KeyCode);
+  };
+
+  render() {
     const index = this.state.selectedIndex;
     const renderThumbs = (uri, i) => <Thumb key={i} source={uri} myIndex={i} selectedIndex={index} />;
     return (
-      <View style={styles.horizontalScrollView}>
-        <ScrollView          
-          scrollEnabled = {false}
-          ref={(scrollView) => {this._scrollView = scrollView;}}                  
+      <View>
+        <ScrollView
+          scrollEnabled={false}
+          ref={(scrollView) => { this._scrollView = scrollView; }}
           automaticallyAdjustContentInsets={false}
           scrollEventThrottle={16}
           horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          style={[styles.scrollView]}>
+          showsHorizontalScrollIndicator={false}>
           {THUMB_URIS.map(renderThumbs)}
-        </ScrollView>        
+        </ScrollView>
       </View>
     );
   }
@@ -180,12 +178,12 @@ export default class JuvoReactNative extends Component {
   toggle() {
     this.setState({
       visible: !this.state.visible
-    });    
+    });
   }
 
-  componentWillMount() {    
+  componentWillMount() {
     JuvoEventEmitter.addListener(
-      'onTVKeyPress', 
+      'onTVKeyPress',
       this.onTVKey
     );
   }
@@ -195,49 +193,65 @@ export default class JuvoReactNative extends Component {
     //pressed.KeyName
     //pressed.KeyCode   
 
-    switch (pressed.KeyName)
-      {
-        case "Return":
-        case "XF86AudioPlay":    
-        case "XF86PlayBack": 
-            JuvoPlayer.log("Start playback...");
-            if (this.state.visible) {
-              //JuvoPlayer.startPlayback();
-              this.toggle();
-            }
-            else {
-              //pause
-              //JuvoPlayer.pauseResumePlayback();
-            }
-            break;
-        case "XF86Back":
-        case "XF86AudioStop":        
-            if (!this.state.visible) {            
-              //JuvoPlayer.stopPlayback();
-              this.toggle();
-            }  
-            break;        
-      }  
+    switch (pressed.KeyName) {
+      case "Return":
+      case "XF86AudioPlay":
+      case "XF86PlayBack":
+        JuvoPlayer.log("Start playback...");
+        if (this.state.visible) {
+          JuvoPlayer.startPlayback();
+          this.toggle();
+        }
+        else {
+          //pause
+          JuvoPlayer.pauseResumePlayback();
+        }
+        break;
+      case "XF86Back":
+      case "XF86AudioStop":
+        if (!this.state.visible) {
+          JuvoPlayer.stopPlayback();
+          this.toggle();
+        }
+        break;
+    }
     //JuvoPlayer.log("JuvoReactNative params - KeyName  " + pressed.KeyName + " the code: " + pressed.KeyCode);
   }
 
   handleSelectedIndexChange(index) {
-    this.setState({selectedClipIndex: index});
-    JuvoPlayer.log("handleSelectedIndexChange index = " + index);
+    this.setState({ selectedClipIndex: index});
+    
+    //this.forceUpdate();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return true;
   }
 
   render() {
+    JuvoPlayer.log("JuvoReactNative render() this.state.selectedClipIndex = " + this.state.selectedClipIndex);
+    const index = this.state.selectedClipIndex;
     return (
       <View style={styles.container}>
-        <HideableView
-          visible={this.state.visible}          
-          style={styles.clip_details}>
-            <Image style={styles.img_big} source={require('./res/images/car.png')} />
-            <Text style={styles.clip_details_text} >
-              {THUMB_URIS[this.state.selectedClipIndex]}
-            </Text>
-            <HorizontalScrollView onSelectedIndexChange={this.handleSelectedIndexChange}/>
-        </HideableView >               
+
+        <HideableView visible={this.state.visible}>
+
+            <View>
+              <Image style={{ width: 1920, height: 1080 }} source={require('./res/images/car.png')} />
+            </View>
+
+            <View style={{ position: 'absolute', top: 300, left: 200 }}>
+              <Text style={{ fontSize: 30, color: '#7fff00'  }}>
+                {THUMB_URIS[index]}
+              </Text>
+            </View>
+
+            <View style={{ top: -300 }}>
+              <HorizontalScrollView style={{ position: 'relative' }} onSelectedIndexChange={this.handleSelectedIndexChange} />
+            </View>
+          
+        </HideableView>
+
       </View>
     );
   }
@@ -262,7 +276,8 @@ class HideableView extends Component {
   }
 
   shouldComponentUpdate(nextProps) {
-    return this.props.visible !== nextProps.visible;
+    //return this.props.visible !== nextProps.visible;
+    return true;
   }
 
   componentWillUpdate(nextProps, nextState) {
@@ -276,9 +291,11 @@ class HideableView extends Component {
       return (this.visible && this.props.children);
     }
     return (
-      <Animated.View style={{ opacity: this.state.opacity }}>
-        {this.props.children}
-      </Animated.View>
+      <View>
+        <Animated.View style={{ opacity: this.state.opacity }}>
+          {this.props.children}
+        </Animated.View>
+      </View>
     )
   }
 }
@@ -292,81 +309,29 @@ HideableView.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: 'transparent',
     width: 1920,
-    height: 1080,
-    top: 0,
-    left: 0, 
-    position: 'absolute'
-  },
-  scrollView: {
-    top: -265,
-    left: 0, 
-    position: 'absolute',    
-    height: 300,
-    width: 1920,
-    backgroundColor: "#adff2f"
-  },
-  horizontalScrollView: {
-    flex: 1,
-    height: '100%',
-    width: 1920,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
+    height: 1080
   },
   thumb: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#cccccc',
     width: 459,
     height: 260
   },
-  thumb_selected : {    
-    position: 'relative',
-    top: 0,
-    left: 0,
-    alignItems: 'center', 
-    justifyContent: 'center',   
+  thumb_selected: {
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#ffd700',
     width: 459,
     height: 260
   },
   img_thumb: {
-    justifyContent: 'center',
-    alignItems: 'center',
     width: '98%',
-    height: '98%',
-  },
-  img_big: {    
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: 1920,
-    height: 1080
-  },   
-  clip_details: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: 1920,
-    height: 1080
-  },
-  clip_details_text: {    
-    position: "relative",
-    top: 500,
-    left: 150,
-    width: '100%',
-    height: '100%',
-    fontSize: 30,
-    color: '#000000'
+    height: '98%'
   }
+
 });
 
 AppRegistry.registerComponent('JuvoReactNative', () => JuvoReactNative);
