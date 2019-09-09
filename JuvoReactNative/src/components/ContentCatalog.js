072a10bfdd1da4a93a8825dd3861c31d20b8075e
p.buczynski <p.buczynski@samsung.com>
2019-09-09 09:44:20 +0200

    bigPicture update fix. Refactoring.

--- JuvoReactNative/src/components/ContentCatalog.js
+++ JuvoReactNative/src/components/ContentCatalog.js
@@ -8,18 +8,18 @@ import {
 
 import HideableView from './HideableView';
 import ContentPicture from './ContentPicture';
-import ContentScrollView from './ContentScrollView';
+import ContentScroll from './ContentScroll';
 import LocalResources from '../LocalResources';
 
 export default class ContentCatalog extends Component {
   
   constructor(props) {
     super(props);
-    this.state = {            
-      bigPictureVisible: true,
+    this.state = {   
       selectedClipIndex : 0
     };    
-    this.visible = this.props.visibility;  
+    this.visible = this.props.visibility; 
+    this.bigPictureVisible = this.visible;
     this.keysListenningOff = false;
     this.toggleVisibility = this.toggleVisibility.bind(this);
     this.onTVKeyDown = this.onTVKeyDown.bind(this);
@@ -29,11 +29,6 @@ export default class ContentCatalog extends Component {
     this.handleBigPicLoadEnd = this.handleBigPicLoadEnd.bind(this);
     this.JuvoPlayer = NativeModules.JuvoPlayer;
     this.JuvoEventEmitter = new NativeEventEmitter(this.JuvoPlayer);
-  }
-
-  toggleVisibility() {    
-   this.visible = !this.visible; 
-   this.props.switchVisibility('PlaybackView', !this.visible);  
   }
 
   componentWillMount() {
@@ -46,13 +41,28 @@ export default class ContentCatalog extends Component {
       this.onTVKeyUp
     );   
   }
+
+  componentDidUpdate(prevProps, prevState) {   
+    this.bigPictureVisible = true;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {  
+    return true;
+  } 
+
+  toggleVisibility() {    
+   this.visible = !this.visible; 
+   this.props.switchView('PlaybackView', !this.visible);  
+  }
+
+  rerender() {
+    this.setState({selectedIndex: this.state.selectedIndex});
+  }
   
   onTVKeyDown(pressed) {
     //There are two parameters available:
     //pressed.KeyName
-    //pressed.KeyCode      
-
-   // this.JuvoPlayer.log("ContentCatalog onTVKeyDown ");
+    //pressed.KeyCode         
     if (this.keysListenningOff) return;    
   
     switch (pressed.KeyName) {     
@@ -60,65 +70,68 @@ export default class ContentCatalog extends Component {
       case "XF86AudioStop":         
       case "Return":
       case "XF86AudioPlay":
-      case "XF86PlayBack":         
-        this.toggleVisibility();              
+      case "XF86PlayBack":                 
+        this.toggleVisibility();
         break;
+      case "Left", "Right":
+                    
+        break;            
+    }    
+   
+    if (this.bigPictureVisible) {      
+      //hide big picture during the fast scrolling (long key press)
+      this.bigPictureVisible = false;  
+      this.rerender();
     }   
-
-    this.setState({bigPictureVisible: false});
+    
   }
-  onTVKeyUp(pressed) {        
+  onTVKeyUp(pressed) {   
     if (this.keysListenningOff) return;
-    this.setState({bigPictureVisible: true});      
+    this.bigPictureVisible = true;    
   }  
 
   handleSelectedIndexChange(index) {     
     this.props.onSelectedIndexChange(index);  
-    this.setState({selectedClipIndex: index});
+    this.setState({selectedClipIndex: index});    
+  }  
+
+  handleBigPicLoadStart() {     
+    this.bigPictureVisible = false; 
   }
 
-  shouldComponentUpdate(nextProps, nextState) {       
-    return true;
-  } 
-
-  handleBigPicLoadStart() {   
-    this.setState({bigPictureVisible: false});
-  }
-
-  handleBigPicLoadEnd() {  
-    this.setState({bigPictureVisible: true });
+  handleBigPicLoadEnd() {    
+    this.bigPictureVisible = true; 
   }
 
   render() {    
     const index = this.state.selectedClipIndex ? this.state.selectedClipIndex : 0;    
     const uri = LocalResources.tileNames[index];
     const path = LocalResources.tilePathSelect(uri);   
-    const overlay = LocalResources.tilesPath.contentDescriptionBackground;
-    const fadeduration = 300;
+    const overlay = LocalResources.tilesPath.contentDescriptionBackground;    
     const visibility = this.props.visibility ? this.props.visibility : this.visible;   
     this.visible = visibility;
     this.keysListenningOff = !visibility; 
-    const showBigPicture = this.state.bigPictureVisible;// ? this.state.bigPictureVisible: true; 
-    this.JuvoPlayer.log("ContentCatalog render() showBigPicture = " + showBigPicture);
+    const showBigPicture = this.bigPictureVisible; 
+   
     return (
       <View style={{backgroundColor: 'transparent'}}>
-        <HideableView  visible={visibility} duration={fadeduration}>
-          <HideableView  visible={showBigPicture} duration={fadeduration} style={{zIndex: -100 }}>          
-            <View style={{position: 'relative', top: 0, left: 650, width: 1270, height: 800, zIndex: -11  }}>
+        <HideableView  visible={visibility} duration={300}>
+          <HideableView  visible={showBigPicture} duration={100} style={{zIndex: -20 }}>          
+            <View style={{top: 0, left: 650, width: 1270, height: 800, zIndex: -11  }}>
               <ContentPicture source={uri} selectedIndex={index} 
                       path={path} onLoadEnd={this.handleBigPicLoadEnd} onLoadStart={this.handleBigPicLoadStart}
-                      width={1266} height={715} top={0} left = {0} position={'relative'}                        
+                      width={1266} height={715} top={0} left = {0}                         
                       />              
             </View>              
           </HideableView> 
-          <View style={{position: 'relative', top: -800, left: 650, width: 1270, height: 800, zIndex: -10 }}>
+          <View style={{ top: -800, left: 650, width: 1270, height: 800, zIndex: -10 }}>
             <ContentPicture  source={uri} selectedIndex={index} 
                       path={overlay}
-                      width={1266} height={715} top={0} left = {0} position={'relative'}                        
+                      width={1266} height={715} top={0} left = {0}                         
                       />
             </View>  
           <View style={{position: 'relative', top: -1600, width: 1920, height: 1080, zIndex: 100 }}>
-            <ContentScrollView onSelectedIndexChange={this.handleSelectedIndexChange} contentURIs={LocalResources.tileNames}  keysListenningOff={this.keysListenningOff}/>
+            <ContentScroll onSelectedIndexChange={this.handleSelectedIndexChange} contentURIs={LocalResources.tileNames}  keysListenningOff={this.keysListenningOff}/>
           </View>   
         </HideableView> 
       </View>
