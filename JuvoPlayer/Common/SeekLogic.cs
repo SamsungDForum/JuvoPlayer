b6d0b6558e8e09305cd5f7594c06fe5f5a61e0f5
Grzegorz Skowinski <g.skowinski@samsung.com>
2019-10-22 14:21:25 +0200

    Changed public Subject to public IObservable.

--- JuvoPlayer/Common/SeekLogic.cs
+++ JuvoPlayer/Common/SeekLogic.cs
@@ -19,6 +19,7 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading;
 using System.Threading.Tasks;
@@ -46,7 +47,12 @@ namespace JuvoPlayer.Common
 
         public bool IsSeekInProgress { get; set; }
         public bool IsSeekAccumulationInProgress { get; set; }
-        public Subject<Unit> SeekCompleted = new Subject<Unit>();
+
+        private Subject<Unit> _seekCompleted = new Subject<Unit>();
+        public IObservable<Unit> SeekCompleted()
+        {
+            return _seekCompleted.AsObservable();
+        }
 
         private readonly Stopwatch _seekStopwatch = new Stopwatch();
         private TimeSpan _targetSeekTime;
@@ -121,7 +127,7 @@ namespace JuvoPlayer.Common
             try
             {
                 await ExecuteSeek();
-                SeekCompleted.OnNext(Unit.Default);
+                _seekCompleted.OnNext(Unit.Default);
             }
             catch (SeekException)
             {
