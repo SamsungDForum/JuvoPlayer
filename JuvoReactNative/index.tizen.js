65cf99f38ef3bd4c0d7135d9275f8c987839f559
Pawel Flis <p.flis@AMDC3317.digital.local>
2020-01-13 14:52:25 +0100

    Implemented NavigationExperimental and changed the way of receiving events from a pilot.

--- JuvoReactNative/index.tizen.js
+++ JuvoReactNative/index.tizen.js
@@ -12,7 +12,9 @@ import {
   AppRegistry,
   NativeModules,
   NativeEventEmitter,
-  Dimensions
+  Dimensions,
+  NavigationExperimental,
+  DeviceEventEmitter
 } from 'react-native';
 
 import ContentCatalog from './src/views/ContentCatalog';
@@ -20,26 +22,66 @@ import PlaybackView from './src/views/PlaybackView';
 import ResourceLoader from "./src/ResourceLoader";
 import InProgressView from "./src/views/InProgressView";
 
+const {
+  CardStack: NavigationCardStack,
+  StateUtils: NavigationStateUtils
+} = NavigationExperimental;
+
+function createReducer(initialState) {
+  return (currentState = initialState, action) => {
+    switch (action.type) {
+      case 'forward':
+        return NavigationStateUtils.forward(currentState);
+      case 'back':
+        return NavigationStateUtils.back(currentState);
+      case 'push':
+        return NavigationStateUtils.push(currentState, {key: action.key});
+      case 'pop':
+        return currentState.index > 0 ?
+          NavigationStateUtils.pop(currentState) :
+          currentState;
+      default:
+        return currentState;
+    }
+  }
+}
+
+const NavReducer = createReducer({
+  index: 0,
+  key: 'App',
+  routes: [{key: 'Catalog'}]
+});
+
 export default class JuvoReactNative extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: true,
-      components: {
-        'isContentCatalogVisible': true,
-        'isPlaybackViewVisible': false
-      },
-      deepLinkIndex: 0
+      navState: NavReducer(undefined, {}),
+      deepLinkIndex: 0,
     };
+    this.currentView = 'ContentCatalog';
     this.selectedClipIndex = 0;
     this.switchComponentsView = this.switchComponentsView.bind(this);
     this.handleSelectedIndexChange = this.handleSelectedIndexChange.bind(this);
     this.handleDeepLink = this.handleDeepLink.bind(this);
+    this.onTVKeyDown = this.onTVKeyDown.bind(this);
+    this.onTVKeyUp = this.onTVKeyUp.bind(this);
     this.JuvoPlayer = NativeModules.JuvoPlayer;
     this.JuvoEventEmitter = new NativeEventEmitter(this.JuvoPlayer);
   }
 
+  onTVKeyDown(pressed) {
+    DeviceEventEmitter.emit(`${this.currentView}/onTVKeyDown`, pressed);
+  }
+
+  onTVKeyUp(pressed) {
+    DeviceEventEmitter.emit(`${this.currentView}/onTVKeyUp`, pressed);
+  }
+
   componentWillMount() {
+    this.JuvoEventEmitter.addListener("onTVKeyDown", this.onTVKeyDown);
+    this.JuvoEventEmitter.addListener("onTVKeyUp", this.onTVKeyUp);
     this.JuvoEventEmitter.addListener("handleDeepLink", this.handleDeepLink);
     this.JuvoPlayer.AttachDeepLinkListener();
   }
@@ -48,20 +90,12 @@ export default class JuvoReactNative extends Component {
   switchComponentsView(componentName) {
     switch (componentName) {
       case 'ContentCatalog':
-        this.setState({
-          components: {
-            'isContentCatalogVisible': true,
-            'isPlaybackViewVisible': false
-          }
-        });
+        this.currentView = 'ContentCatalog';
+        this._handleAction({type: 'pop'});
         break;
       case 'PlaybackView':
-        this.setState({
-          components: {
-            'isContentCatalogVisible': false,
-            'isPlaybackViewVisible': true
-          }
-        });
+        this.currentView = 'PlaybackView';
+        this._handleAction({type: 'push', key: 'Playback'});
         break;
     }
   }
@@ -83,6 +117,46 @@ export default class JuvoReactNative extends Component {
     this.setState({loading: false});
   }
 
+  _handleAction(action) {
+    const newState = NavReducer(this.state.navState, action);
+    if (newState === this.state.navState) {
+      return false;
+    }
+    this.setState({
+      navState: newState
+    });
+    return true;
+  }
+
+  handleBackAction() {
+    return this._handleAction({type: 'pop'});
+  }
+
+  _renderRoute(key) {
+    if (key === 'Catalog') {
+      return <ContentCatalog
+        visibility={true}
+        switchView={this.switchComponentsView}
+        stateView={this.currentView}
+        onSelectedIndexChange={this.handleSelectedIndexChange}
+        deepLinkIndex={this.state.deepLinkIndex}/>
+    }
+    if (key === 'Playback') {
+      return <PlaybackView
+        visibility={true}
+        stateView={this.currentView}
+        switchView={this.switchComponentsView}
+        selectedIndex={this.selectedClipIndex}/>
+    }
+  }
+
+  _renderScene(props) {
+    const ComponentToRender = this._renderRoute(props.scene.route.key);
+    return (
+      ComponentToRender
+    );
+  }
+
   render() {
     if (this.state.loading) {
       return (
@@ -92,17 +166,11 @@ export default class JuvoReactNative extends Component {
       );
     } else {
       return (
-        <View style={styles.container}>
-          <ContentCatalog
-            visibility={this.state.components.isContentCatalogVisible}
-            switchView={this.switchComponentsView}
-            onSelectedIndexChange={this.handleSelectedIndexChange}
-            deepLinkIndex={this.state.deepLinkIndex}/>
-          <PlaybackView
-            visibility={this.state.components.isPlaybackViewVisible}
-            switchView={this.switchComponentsView}
-            selectedIndex={this.selectedClipIndex}/>
-        </View>
+        <NavigationCardStack
+          cardStyle={{backgroundColor: 'transparent'}}
+          navigationState={this.state.navState}
+          onNavigate={this._handleAction.bind(this)}
+          renderScene={this._renderScene.bind(this)}/>
       );
     }
   }
