5a2649d24edcf61818e87d2319d0ee577b2fe155
Pawel Buczynski <p.buczynski@AVDC720.digital.local>
2020-06-22 13:53:50 +0200

    user certificates test for CI building

--- bamboo-specs/build_and_test.sh
+++ bamboo-specs/build_and_test.sh
@@ -16,7 +16,8 @@ echo "CERTS_PATH=${CERTS_PATH}"
 
 try dotnet sln remove JuvoReactNative/Tizen/JuvoReactNative.csproj
 
-try dotnet build /nodeReuse:false /p:"AuthorPath=${CERTS_PATH}/partner_2019.p12;AuthorPass=${bamboo_AuthorPassword}" /p:"DistributorPath=${CERTS_PATH}/tizen-distributor-signer.p12;DistributorPass=${bamboo_DistributorPassword}"
+#try dotnet build /nodeReuse:false /p:"AuthorPath=${CERTS_PATH}/partner_2019.p12;AuthorPass=${bamboo_AuthorPassword}" /p:"DistributorPath=${CERTS_PATH}/tizen-distributor-signer.p12;DistributorPass=${bamboo_DistributorPassword}"
+try dotnet build /nodeReuse:false /p:"AuthorPath=/home/p.buczynski/SamsungCertificate/t55_linux_local/author.p12;AuthorPass=1234567890" /p:"DistributorPath=/home/p.buczynski/SamsungCertificate/t55_linux_local/distributor.p12;DistributorPass=1234567890"
 
 try dotnet test /nodeReuse:false JuvoPlayer.Tests/JuvoPlayer.Tests.csproj --logger:trx -f netcoreapp2.0
 
