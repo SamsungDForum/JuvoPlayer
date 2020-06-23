78e1bc3f7f7c5ced512aa7bd466d6b55f9dcaaa4
Pawel Buczynski/Multichannel Video Services (VD) /SRPOL/Staff Engineer/Samsung Electronics <p.buczynski@samsung.com>
2020-06-23 17:13:20 +0200

    bamboo build and test script restored

--- bamboo-specs/build_and_test.sh
+++ bamboo-specs/build_and_test.sh
@@ -14,12 +14,11 @@ echo "SCRIPT=${SCRIPT}"
 echo "SCRIPT_PATH=${SCRIPT_PATH}"
 echo "CERTS_PATH=${CERTS_PATH}"
 
-#try dotnet sln remove JuvoReactNative/Tizen/JuvoReactNative.csproj
+try dotnet sln remove JuvoReactNative/Tizen/JuvoReactNative.csproj
 
-#try dotnet build /nodeReuse:false /p:"AuthorPath=${CERTS_PATH}/partner_2019.p12;AuthorPass=${bamboo_AuthorPassword}" /p:"DistributorPath=${CERTS_PATH}/tizen-distributor-signer.p12;DistributorPass=${bamboo_DistributorPassword}"
-try dotnet build /nodeReuse:false /p:"AuthorPath=/home/p.buczynski/SamsungCertificate/t55_linux_local/author.p12;AuthorPass=1234567890" /p:"DistributorPath=/home/p.buczynski/SamsungCertificate/t55_linux_local/distributor.p12;DistributorPass=1234567890"
+try dotnet build /nodeReuse:false /p:"AuthorPath=${CERTS_PATH}/partner_2019.p12;AuthorPass=${bamboo_AuthorPassword}" /p:"DistributorPath=${CERTS_PATH}/tizen-distributor-signer.p12;DistributorPass=${bamboo_DistributorPassword}"
 
 try dotnet test /nodeReuse:false JuvoPlayer.Tests/JuvoPlayer.Tests.csproj --logger:trx -f netcoreapp2.0
 
-#echo "Cyclomatic complexity results:"
-#CCM.exe bamboo-specs/ccm.config | tee CyclomaticComplexityReport.log
+echo "Cyclomatic complexity results:"
+CCM.exe bamboo-specs/ccm.config | tee CyclomaticComplexityReport.log
